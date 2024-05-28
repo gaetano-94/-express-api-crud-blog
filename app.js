da504b1b067cb32fdb3fcdf700e82e1c35b6971c
gaetano-94 <gaetano.panico1994@gmail.com>
2024-05-28 15:42:06 +0200

    feat: middleware globale

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const postsRouter = require('./routers/posts');
 app.use(express.static('./public'));
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 });
 
 app.use('/posts', postsRouter);
+
+// Middleware globale per gestire gli errori
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Errore interno del server');
+});
 
 app.listen(port, () => {
   console.log(`Server avviato su http://localhost:${port}`);
